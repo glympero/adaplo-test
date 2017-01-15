ce26602d12ecc3bee0eebbd3a538de98ffbe94c0
Georgios Lymperopoulos <glympe77@gmail.com>
2017-01-15 03:13:01 +0200

    Code refactoring

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,55 +1,58 @@
 require 'createsend'
 
 class PagesController < ApplicationController
+  before_action :set_api
+  before_action :set_list_id, only: [:add_user, :remove_user]
 
   def home
-    
-    auth = {api_key: '07a50c2d0c1776b10edf3bf429810814'}
     @list_id = '';
-
-    createsend = CreateSend::CreateSend.new(auth)
-
+    createsend = CreateSend::CreateSend.new(@auth)
     createsend.clients.each do |item|
       client_name = item.Name
-      client = CreateSend::Client.new(auth, item.ClientID)
+      client = CreateSend::Client.new(@auth, item.ClientID)
       client.lists.each do |item|
-        # puts "#{item.ListID} #{client_name} #{item.Name}"
         @list_name = item.Name
         @list_id = item.ListID
       end
     end
-    end
+  end
 
-    def add_user
-      auth = {api_key: '07a50c2d0c1776b10edf3bf429810814'}
-      list_id = '81740a3f63f6584900137d3600ba508b';
-      email, name = params[:email], params[:name]
+  def add_user
+    email, name = params[:email], params[:name]
 
-      begin
-        CreateSend::Subscriber.add(auth, list_id, email, name, [], false)
-        flash[:success] = "Subscriber added successfully"
-        redirect_to root_path
-      rescue CreateSend::BadRequest => exception
-        flash[:danger] = exception.message
-        redirect_to root_path
-        # fail "could not add #{email} code=#{exception.data.Code}"
-      end
+    begin
+      CreateSend::Subscriber.add(@auth, @list_id, email, name, [], false)
+      flash[:success] = "Subscriber added successfully"
+      to_home()
+    rescue CreateSend::BadRequest => exception
+      flash[:danger] = exception.message
+      to_home()
     end
+  end
 
     def remove_user
-      auth = {api_key: '07a50c2d0c1776b10edf3bf429810814'}
-      list_id = '81740a3f63f6584900137d3600ba508b';
-      #REMOVE
       email = params[:email_remove]
 
       begin
-        CreateSend::Subscriber.new(auth, list_id, email).unsubscribe
+        CreateSend::Subscriber.new(@auth, @list_id, email).unsubscribe
         flash[:success] = "Subscriber deleted successfully"
-        redirect_to root_path
+        to_home()
       rescue CreateSend::BadRequest => exception
         flash[:danger] = exception.message
-        redirect_to root_path
-        # fail "could not unsubscribe #{email} code=#{exception.data.Code}"
+        to_home()
       end
     end
+
+    private
+      def set_api
+        @auth = {api_key: '07a50c2d0c1776b10edf3bf429810814'}
+      end
+
+      def set_list_id
+        @list_id = '81740a3f63f6584900137d3600ba508b';
+      end
+
+      def to_home
+        redirect_to root_path
+      end
 end
